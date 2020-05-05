e9e68eb5291b848f7e92f196e9ff2601efd7aaa9
Harishwarrior <warriorharish95668@gmail.com>
2020-05-05 08:48:18 +0530

    Initial commit

---
+++ app/src/main/java/com/search/tr/ContactsAdapter.java
@@ -0,0 +1,116 @@
+package com.search.tr;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ravi on 16/11/17.
+ */
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder>
+        implements Filterable {
+    private List<Contact> contactList;
+    private List<Contact> contactListFiltered;
+    private ContactsAdapterListener listener;
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, magnets,url;
+
+
+        MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.name);
+            url = view.findViewById(R.id.url);
+
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // send selected contact in callback
+                    listener.onContactSelected(contactListFiltered.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+
+
+    ContactsAdapter(List<Contact> contactList, ContactsAdapterListener listener) {
+        this.listener = listener;
+        this.contactList = contactList;
+        this.contactListFiltered = contactList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_row_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final Contact contact = contactListFiltered.get(position);
+        holder.name.setText(contact.getName());
+//        holder.magnets.setText(contact.getMagnets());
+        holder.url.setText(contact.getUrl());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactListFiltered==null ? 0:contactListFiltered.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    contactListFiltered = contactList;
+                } else {
+                    List<Contact> filteredList = new ArrayList<>();
+                    for (Contact row : contactList) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getName().toLowerCase().contains(charString.toLowerCase()) || row.getUrl().contains(charSequence)) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    contactListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = contactListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                contactListFiltered = (ArrayList<Contact>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public interface ContactsAdapterListener {
+        void onContactSelected(Contact contact);
+    }
+}
---
+++ app/src/main/java/com/search/tr/MyApplication.java
@@ -0,0 +1,41 @@
+package com.search.tr;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MyApplication extends Application {
+
+    public static final String TAG = MyApplication.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+}
